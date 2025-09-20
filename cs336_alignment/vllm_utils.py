e90acdb39a14cd487d7f7f7e9f356a6c2b0ee68a
wheatwaves <wheatwaves3@gmail.com>
2025-09-19 21:01:59 -0400

    add

--- cs336_alignment/vllm_utils.py
+++ cs336_alignment/vllm_utils.py
@@ -22,3 +22,29 @@ def load_policy_into_vllm_instance(policy: PreTrainedModel, llm: LLM):
     state_dict = policy.state_dict()
     llm_model = llm.llm_engine.model_executor.driver_worker.model_runner.model
     llm_model.load_weights(state_dict.items())
+
+def evaluate_vllm(vllm_model, reward_fn, prompts, sampling_params, ground_truth):
+    outputs = vllm_model.generate(prompts, sampling_params)
+    evaluation_data = []
+    format_reward = 0
+    answer_reward = 0
+    total_reward = 0
+
+    for output, ground_truth in zip(outputs, ground_truth):
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        reward = reward_fn(generated_text, ground_truth)
+        data = {
+            "prompt": prompt,
+            "generated_text": generated_text,
+            "reward": reward,
+        }
+        evaluation_data.append(data)
+        format_reward += reward["format_reward"]
+        answer_reward += reward["answer_reward"]
+        total_reward += reward["reward"]
+    print(f"Number of prompts: {len(outputs)}")
+    print(f"Format reward: {format_reward / len(outputs)}")
+    print(f"Answer reward: {answer_reward / len(outputs)}")
+    print(f"Reward: {total_reward / len(outputs)}")
+    json.dump(evaluation_data, open("evaluation_epoch_2_data.json", "w"))
